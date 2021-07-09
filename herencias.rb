b58d1959c10c172fc2139a9bd35c4ec0232f5fd8
Flavio <flavio.cardemil@gmail.com>
2021-07-08 20:05:33 -0400

    final

--- herencias.rb
+++ herencias.rb
@@ -1,42 +1,65 @@
 class Appointment
     attr_reader :location, :purpose, :hour, :min
+
     def initialize(location,purpose,hour,min)
-        @location = location
-        @purpose = purpose
-        @hour = hour
-        @min = min
+        get_location_purpose(location,purpose)
+        get_hour_min(hour,min)
     end
+
+    def get_location_purpose(location,purpose)
+        if location.class == String && purpose.class == String
+            @location = location
+            @purpose = purpose
+        else
+            raise "Pásate un string"
+        end
+    end
+
+    
+    def get_hour_min(hour,min)
+        if hour.class == Integer && min.class == Integer
+            @hour = hour
+            @min = min
+        else
+            raise "Pásate un número"
+        end
+    end
+
 end
 
 class MonthlyAppointment < Appointment
     attr_reader :day
+
     def initialize(location,purpose,hour,min,day)
         super(location,purpose,hour,min)
         @day = day
     end
 
     def occurs_on?(day)
-        @day == day
+        self.day == day
     end
 
     def to_s
-        "Reunión mensual en #{@location} sobre #{@purpose} el día #{@day} a la(s) #{@hour}:#{@min}.."
+        "Reunión mensual en #{self.location} sobre #{self.purpose} el día #{self.day} a la(s) #{self.hour}:#{self.min}."
     end
+
 end
 
 class DailyAppointment < Appointment
+
     def to_s
-        "Reunión diaria en #{@location} sobre #{@purpose} a las #{@hour}:#{@min}"
+        "Reunión diaria en #{self.location} sobre #{self.purpose} a las #{self.hour}:#{self.min}"
     end
 
     def occurs_on?(hour,min)
-         @hour == hour && @min == min
+         self.hour == hour && self.min == min
     end
 
 end
 
 class OneTimeAppointment < Appointment
     attr_reader :day, :month, :year
+
     def initialize(location,purpose,hour,min,day,month,year)
         super(location,purpose,hour,min)
         @day = day
@@ -45,12 +68,13 @@ class OneTimeAppointment < Appointment
     end
 
     def to_s
-        "Reunión única en #{@location} sobre #{@purpose} el #{@day}/#{@month}/#{@year} a las #{@hour}:#{@min}"
+        "Reunión única en #{self.location} sobre #{self.purpose} el #{self.day}/#{self.month}/#{self.year} a las #{self.hour}:#{self.min}"
     end
 
     def occurs_on?(day,month,year)
-        @day == day && @month == month && @year == year
-   end
+        self.day == day && self.month == month && self.year == year
+    end
+
 end
 
 a = OneTimeAppointment.new('Desafío Latam', 'Trabajo', 14, 30, 4, 6, 2019)
@@ -60,4 +84,4 @@ b = DailyAppointment.new('Desafío Latam', 'Educación', 8, 15)
 c = MonthlyAppointment.new('NASA', 'Aliens', 8, 15, 23)
 # Reunión mensual en NASA sobre Aliens el día 23 a la(s) 8:15
 
-pp a.occurs_on?(8,15,6)
+puts c
